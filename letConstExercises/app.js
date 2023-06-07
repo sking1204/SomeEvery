c553d8e2c342fb5ef6d7dbf26d16c9efb7dbe084
Shanaz King <sking1128@YewYew1128.localdomain>
2023-06-07 18:41:54 -0400

    adding js

---
+++ letConstExercises/app.js
@@ -0,0 +1,38 @@
+/* ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+ES2015 Global Constants
+Write an ES2015 Version
+
+*/
+const PI = 3.14
+PI=42
+
+/*
+
+1)- What is the difference between ***var*** and ***let***?
+Var declarations are globally scoped, can be updated and redeclared..
+Let declaratioins are block scoped, can be updated, but not redeclared.
+
+
+2)- What is the difference between **var** and ***const***?
+Var is globally scoped, const is block scoped, can be updated and redeclared.
+Const is block scoped, cannot be updated or redeclared. 
+
+
+3)- What is the difference between **let** and **const**?
+Let can be reassigned but not redeclared.
+Const cannot be reassigned or redeclared.
+
+
+
+4)- What is hoisting?
+The declaration of the variable and functions are moved to the top of the current script/function.
+
+*/
+
+
+
+
